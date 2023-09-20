b3c7da0f58ec42cc3aa6bab50f18b2a709d8ceb8
Fallah Andy Prakasa <me@flla.my.id>
2023-09-20 21:40:16 +0700

    use `useVisibleTask$` instead of `useClientEffect$` useClientEffect is already deprecated

--- src/lottie.tsx
+++ src/lottie.tsx
@@ -1,4 +1,4 @@
-import { component$, useClientEffect$, useStore, useSignal, noSerialize } from '@builder.io/qwik';
+import { component$, useVisibleTask$, useStore, useSignal, noSerialize } from '@builder.io/qwik';
 import lottie from 'lottie-web';
 import { Options } from './types';
 
@@ -10,19 +10,21 @@ export const QwikLottie = component$(({ options }: OptionsProps) => {
   const store = useStore({
     anim: noSerialize({}),
   });
-  const canvas   = useSignal<Element>();
+  const canvas = useSignal<Element>();
 
-  useClientEffect$(() => {
-    store.anim = noSerialize(lottie.loadAnimation({
-      container: options.container || canvas.value,
-      renderer: options.renderer || 'svg',
-      loop: options.loop || true,
-      autoplay: options.autoplay || true,
-      animationData: options.animationData,
-      path: options.path,
-      rendererSettings: options.rendererSettings,
-      name: options.name,
-    }));
+  useVisibleTask$(() => {
+    store.anim = noSerialize(
+      lottie.loadAnimation({
+        container: options.container || canvas.value,
+        renderer: options.renderer || 'svg',
+        loop: options.loop || true,
+        autoplay: options.autoplay || true,
+        animationData: options.animationData,
+        path: options.path,
+        rendererSettings: options.rendererSettings,
+        name: options.name,
+      })
+    );
   });
 
   return <div ref={canvas}></div>;
